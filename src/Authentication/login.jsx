216d5b40a350417253ec85559f9bb6e131c3994c
MTahir27 <m.tahiridrees27@gmail.com>
2022-05-16 22:43:09 +0500

    Login Complete

--- src/Authentication/login.jsx
+++ src/Authentication/login.jsx
@@ -1,15 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, Row, Col, Button } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { InputField } from "../Components/InputField";
+import { auth } from "../Config/Firebase";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 export const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  let navigate = useNavigate();
+  const loginUser = (e) => {
+    e.preventDefault();
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        navigate("/");
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode, errorMessage);
+      });
+  };
   return (
     <div className="container">
       <section className="d-flex justify-content-center align-item-center py-5">
         <Row>
           <Col md={9} lg={8} xl={7} className="mx-auto">
-            <Form className="border p-4 rounded-3 bg-light">
+            <Form
+              className="border p-4 rounded-3 bg-light"
+              onSubmit={loginUser}
+            >
               <h3 className="mb-4 text-dark">Login Form</h3>
               <Row className="g-4">
                 <Col xs={12}>
@@ -18,6 +41,9 @@ export const Login = () => {
                     id="email"
                     placeholder="Email"
                     type="email"
+                    onChange={(e) => {
+                      setEmail(e.target.value);
+                    }}
                   />
                 </Col>
 
@@ -27,6 +53,9 @@ export const Login = () => {
                     id="password"
                     placeholder="Password"
                     type="password"
+                    onChange={(e) => {
+                      setPassword(e.target.value);
+                    }}
                   />
                 </Col>
 
